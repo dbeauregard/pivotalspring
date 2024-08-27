a119de0600538acb037a83c5ed5fe55c635a41e0
Derek Beauregard <dbeauregard@gmail.com>
2024-08-27 09:25:43 -0600

    Initial commit of source from Spring Initializr

---
+++ src/main/java/io/dbeauregard/pivotalspring/PivotalspringApplication.java
@@ -0,0 +1,13 @@
+package io.dbeauregard.pivotalspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PivotalspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PivotalspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/dbeauregard/pivotalspring/PivotalspringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dbeauregard.pivotalspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PivotalspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

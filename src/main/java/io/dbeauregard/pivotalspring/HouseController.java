bdc887e1d2325a8362a0d78716f015849503c5f7
Derek Beauregard <dbeauregard@gmail.com>
2024-08-27 16:55:48 -0600

    Add get by ID controller method and cleanup

--- src/main/java/io/dbeauregard/pivotalspring/HouseController.java
+++ src/main/java/io/dbeauregard/pivotalspring/HouseController.java
@@ -1,13 +1,16 @@
 package io.dbeauregard.pivotalspring;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 @RestController
@@ -18,8 +21,30 @@ class HouseController {
     @Autowired
     private HouseRepository repo;
 
+    @PostMapping("/house/{id}")
+    void addHouse(@PathVariable Long id) {
+        log.info("Called addHouse() on {}.", id);
+    }
+
+    @PutMapping("/house/{id}")
+    void updateHouse(@PathVariable Long id) {
+        log.info("Called updateHouse() on {}.", id);
+    }
+
+    @GetMapping("/house/{id}")
+    ResponseEntity<HouseEntity> getHouse(@PathVariable Long id) {
+        log.info("Called getHouse() on {}.", id);
+        return ResponseEntity.ok(repo.findById(id).orElse(null));
+        // return repo.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("/house/{id}")
+    void deleteHouse(@PathVariable Long id) {
+        log.info("Called deleteHouse() on {}.", id);
+    }
+
     @GetMapping("/houses")
-    Iterable<HouseEntity> getHouses() {
+    Iterable<HouseEntity> getAllHouses() {
         log.info("Called getHouses()...");
         return repo.findAll();
     }

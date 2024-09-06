5fdc1c4463de2984914eaabf873bfe3c4b4c8ee9
Derek Beauregard <dbeauregard@gmail.com>
2024-09-06 14:13:14 -0600

    Smoke test House Repo

--- src/test/java/io/dbeauregard/pivotalspring/HouseControllerTest.java
+++ src/test/java/io/dbeauregard/pivotalspring/HouseControllerTest.java
@@ -1,6 +1,5 @@
 package io.dbeauregard.pivotalspring;
 
-import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
--- src/test/java/io/dbeauregard/pivotalspring/HouseRepositoryTest.java
+++ src/test/java/io/dbeauregard/pivotalspring/HouseRepositoryTest.java
@@ -1,5 +1,9 @@
 package io.dbeauregard.pivotalspring;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -7,10 +11,23 @@ public class HouseRepositoryTest {
 
     private final HouseRepository repo;
 
+    @Autowired
     HouseRepositoryTest(HouseRepository repo) {
         this.repo = repo;
     }
 
-    //Smoke Test Entity and wiring to in memory database.  Not testing Spring JPA/Hybernate.
+    // Smoke Test Entity and wiring to in memory database. Not testing Spring
+    // JPA/Hybernate.
+    @Test
+    public void smokeTestRepo() {
+        HouseEntity result = repo.save(new HouseEntity("1234", 56789));
+        assertNotNull(result.getId());
+        assertNotNull(repo.findById(result.getId()));
+        result.setAddress("5678");
+        result = repo.save(new HouseEntity("1234", 56789));
+        assertNotNull(repo.findAll());
+        assertNotNull(result.getId());
+        repo.delete(result);
+    }
 
 }

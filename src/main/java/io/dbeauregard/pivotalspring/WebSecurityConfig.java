eef6ee8c1ff281bc38272ef75b9009ab0f6c0426
Derek Beauregard <dbeauregard@gmail.com>
2024-09-13 11:16:23 -0600

    move users to users.proprties and print on html page

--- src/main/java/io/dbeauregard/pivotalspring/WebSecurityConfig.java
+++ src/main/java/io/dbeauregard/pivotalspring/WebSecurityConfig.java
@@ -1,17 +1,18 @@
 package io.dbeauregard.pivotalspring;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.provisioning.UserDetailsManagerResourceFactoryBean;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -30,14 +31,8 @@ public class WebSecurityConfig {
         return http.build();
     }
 
-    // @formatter:off
 	@Bean
-	public InMemoryUserDetailsManager userDetailsService() {
-		UserDetails user = User.withDefaultPasswordEncoder()
-				.username("user")
-				.password("password")
-				.roles("USER")
-				.build();
-		return new InMemoryUserDetailsManager(user);
+	public InMemoryUserDetailsManager userDetailsService() throws Exception {
+        return UserDetailsManagerResourceFactoryBean.fromResourceLocation("classpath:users.properties").getObject();
 	}
 }
--- src/main/java/io/dbeauregard/pivotalspring/webmvc/GreetingController.java
+++ src/main/java/io/dbeauregard/pivotalspring/webmvc/GreetingController.java
@@ -1,5 +1,9 @@
 package io.dbeauregard.pivotalspring.webmvc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +12,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
+    private static Logger log = LoggerFactory.getLogger(GreetingController.class);
+
     @GetMapping("/greeting")
     public String getGreeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
-            Model model) {
+            Model model, Authentication auth) {
+        UserDetails userDetails = (UserDetails) auth.getPrincipal();
+        log.info("/greeting called with principal {}", userDetails.getUsername());
         model.addAttribute("name", name);
+        model.addAttribute("username", userDetails.getUsername());
+        model.addAttribute("roles", userDetails.getAuthorities());
         return "greeting";
     }
 

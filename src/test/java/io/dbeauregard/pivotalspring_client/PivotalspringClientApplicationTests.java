78caaee457f688b31700cef02657ec3e12dfdead
Derek Beauregard <dbeauregard@gmail.com>
2024-09-16 17:50:11 -0600

    basic test

--- src/test/java/io/dbeauregard/pivotalspring_client/PivotalspringClientApplicationTests.java
+++ src/test/java/io/dbeauregard/pivotalspring_client/PivotalspringClientApplicationTests.java
@@ -1,13 +1,21 @@
 package io.dbeauregard.pivotalspring_client;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
 class PivotalspringClientApplicationTests {
 
+	@Autowired
+	private RestTemplate restTemplate;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		assertThat(restTemplate).isNotNull();
 	}
 
 }

9babaecf71b34cf57849be43f518970e4afa9533
Derek Beauregard <dbeauregard@gmail.com>
2024-09-16 14:03:55 -0600

    initial commit from start.spring.io

---
+++ src/test/java/io/dbeauregard/pivotalspring_client/PivotalspringClientApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dbeauregard.pivotalspring_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PivotalspringClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

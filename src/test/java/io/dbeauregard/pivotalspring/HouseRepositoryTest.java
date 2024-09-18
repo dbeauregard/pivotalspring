c84f440693230feec9f405552605544bce656796
Derek Beauregard <dbeauregard@gmail.com>
2024-09-18 14:27:06 -0600

    change to DataJpaTest

--- src/test/java/io/dbeauregard/pivotalspring/HouseRepositoryTest.java
+++ src/test/java/io/dbeauregard/pivotalspring/HouseRepositoryTest.java
@@ -4,12 +4,17 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
-@SpringBootTest
+// @SpringBootTest
+@DataJpaTest
 public class HouseRepositoryTest {
 
     private final HouseRepository repo;
+
+	@Autowired
+	private TestEntityManager entityManager;
 
     @Autowired
     HouseRepositoryTest(HouseRepository repo) {

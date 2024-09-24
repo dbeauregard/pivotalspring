e898bc3e7aae0e0d8860e0a548bebd0b74da9e65
Derek Beauregard <dbeauregard@gmail.com>
2024-09-24 16:57:07 -0600

    move AI calls to simple web/html page

--- src/main/java/io/dbeauregard/pivotalspring/ai/OllamaClientController.java
+++ src/main/java/io/dbeauregard/pivotalspring/ai/OllamaClientController.java
@@ -3,11 +3,12 @@ package io.dbeauregard.pivotalspring.ai;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class OllamaClientController {
 
     private final OllamaChatModel chatModel;
@@ -18,10 +19,13 @@ public class OllamaClientController {
     }
 
     @GetMapping("/ai")
-    public String getAI(@RequestParam(name = "msg", required = false, defaultValue = "What can I ask?") String msg)
+    public String getAI(@RequestParam(name = "msg", required = false, defaultValue = "What can I ask?") String msg,
+            Model model)
             throws Exception {
+        log.info("AI Called with: {}", msg);
         String result = chatModel.call(msg);
         log.info("AI Result: {}", result);
-        return result;
+        model.addAttribute("result", result);
+        return "ai";
     }
 }

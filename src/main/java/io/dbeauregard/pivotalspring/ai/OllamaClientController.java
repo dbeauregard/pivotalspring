211e79cf445d950e7c13edbc6c5511a25617902e
Derek Beauregard <dbeauregard@gmail.com>
2024-09-26 11:27:37 -0600

    Switch from spring chatmodel to chatclient and lamma3.2

--- src/main/java/io/dbeauregard/pivotalspring/ai/OllamaClientController.java
+++ src/main/java/io/dbeauregard/pivotalspring/ai/OllamaClientController.java
@@ -2,27 +2,25 @@ package io.dbeauregard.pivotalspring.ai;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.ai.chat.model.ChatModel;
-import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @Controller
 public class OllamaClientController {
 
-    private final ChatModel chatModel;
+    private final ChatClient chatClient;
     private static final Logger log = LoggerFactory.getLogger(OllamaClientController.class);
 
-    public OllamaClientController(OllamaChatModel chatModel) {
-        this.chatModel = chatModel;
+    public OllamaClientController(ChatClient.Builder builder) {
+        this.chatClient = builder.defaultSystem("talk like a drunken pirate").build();
     }
 
     // @ModelAttribute("allFeatures")
     // public List<Message> populateFeatures() {
-    //     return Arrays.asList(Message.ALL);
+    // return Arrays.asList(Message.ALL);
     // }
 
     @GetMapping("/ai")
@@ -30,14 +28,14 @@ public class OllamaClientController {
             Model model)
             throws Exception {
         log.info("AI Called with: {}", msg);
-        
-        //Only call Model if a message is suplied, else respond with default response
+
+        // Only call Model if a message is suplied, else respond with default response
         String result = "Type you message above and click submit.";
-        if(msg != null) {
-            result = chatModel.call(msg);    
+        if (msg != null) {
+            result = chatClient.prompt().user(msg).call().content();
             log.info("AI Result: {}", result);
         }
-        
+
         model.addAttribute("result", result);
         model.addAttribute("message", new Message());
         return "ai";

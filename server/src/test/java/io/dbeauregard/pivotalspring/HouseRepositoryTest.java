d6bb439eb8bb1b3e27d8ddb5e5cc5edc3e7e93bb
Derek Beauregard <dbeauregard@gmail.com>
2025-01-08 16:27:01 -0700

    update repo smoke test

--- server/src/test/java/io/dbeauregard/pivotalspring/HouseRepositoryTest.java
+++ server/src/test/java/io/dbeauregard/pivotalspring/HouseRepositoryTest.java
@@ -1,21 +1,21 @@
 package io.dbeauregard.pivotalspring;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-// import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.testcontainers.shaded.com.google.common.collect.Iterables;
 
-// @SpringBootTest
 @DataJpaTest
 public class HouseRepositoryTest {
 
     private final HouseRepository repo;
 
-    //TODO, future use?
-	// @Autowired
-	// private TestEntityManager entityManager;
+    @Autowired
+    private TestEntityManager entityManager;
 
     @Autowired
     HouseRepositoryTest(HouseRepository repo) {
@@ -26,13 +26,25 @@ public class HouseRepositoryTest {
     // JPA/Hybernate.
     @Test
     public void smokeTestRepo() {
+        //Find All
+        HouseEntity he = new HouseEntity("5678", 1234, 3, 2, 2000);
+        entityManager.persist(he);
+        Iterable<HouseEntity> resultList = repo.findAll();
+        assertNotNull(resultList);
+        assertEquals(Iterables.size(resultList),11);
+        
+        //Save One
         HouseEntity result = repo.save(new HouseEntity("1234", 56789, 3, 2, 2000));
         assertNotNull(result.getId());
         assertNotNull(repo.findById(result.getId()));
+
+        //Update One
         result.setAddress("5678");
         result = repo.save(new HouseEntity("1234", 56789, 2, 2, 1500));
         assertNotNull(repo.findAll());
         assertNotNull(result.getId());
+        
+        //Delete One
         repo.delete(result);
     }
 

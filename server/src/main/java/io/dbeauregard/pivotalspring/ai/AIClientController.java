f01a78003296c40802315b3c8cb6dd36e9095ef3
Derek Beauregard <dbeauregard@gmail.com>
2025-07-17 14:14:25 -0600

    updates to AI structured output

--- server/src/main/java/io/dbeauregard/pivotalspring/ai/AIClientController.java
+++ server/src/main/java/io/dbeauregard/pivotalspring/ai/AIClientController.java
@@ -21,6 +21,7 @@ public class AIClientController {
     @Autowired(required = false) // Make this optional for testing and so page loads
     private ChatClient chatClient;
     private static final Logger log = LoggerFactory.getLogger(AIClientController.class);
+    private static final String structuredDefault = "What alubms did the Smashing Pumpkins release.  Give me the top 10.";
 
     public AIClientController() {
     }
@@ -50,23 +51,21 @@ public class AIClientController {
     }
 
     @GetMapping("/ai/structured")
-    public String getHouse(@RequestParam(name = "message", required = false) String msg,
+    public String getHouse(
+            @RequestParam(name = "message", required = false, defaultValue = structuredDefault) String msg,
             Model model)
             throws Exception {
-        log.info("AI Structured Output Called with: {}", msg);
 
-        // Only call Model if a message is suplied, else respond with default response
-        String result = "Type you message above and click submit.";
-        model.addAttribute("result", result);
-        if (msg != null) {
-            Assert.notNull(chatClient, "ChatClient not configured (null).");
-            List<House> hl = chatClient.prompt().user(msg).call().entity(new ParameterizedTypeReference<List<House>>() {
-            });
-            if (hl != null) {
-                hl.forEach(h -> log.info("AI Result: {}", h));
-                model.addAttribute("result", hl);
-            }
-        }
+                log.info("AI Structured Output Called with: {}", msg);
+
+        Assert.notNull(chatClient, "ChatClient not configured (null).");
+        Album a = chatClient.prompt().user("What is the top selling Smashing Pumpkins Album?").call().entity(Album.class);
+        model.addAttribute("result", a);
+        // List<Album> al = chatClient.prompt().user(msg).call().entity(new ParameterizedTypeReference<List<Album>>() {});
+        // if (al != null) {
+        //     al.forEach(h -> log.info("AI Result: {}", h));
+        //     model.addAttribute("result", al);
+        // }
 
         model.addAttribute("message", new Message());
         return "ai";
@@ -86,6 +85,6 @@ public class AIClientController {
         return "ai";
     }
 
-    record House(String address, String price, String sqft, String bdrm, String bath) {
+    record Album(String name, String Artist, String year) {
     };
 }

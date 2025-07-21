25d5b72051f487a472e3053b5c5db6962ef3b330
Derek Beauregard <dbeauregard@gmail.com>
2025-07-21 11:14:49 -0600

    remove unused imports

--- server/src/main/java/io/dbeauregard/pivotalspring/ai/AIClientController.java
+++ server/src/main/java/io/dbeauregard/pivotalspring/ai/AIClientController.java
@@ -1,12 +1,9 @@
 package io.dbeauregard.pivotalspring.ai;
 
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.Assert;
